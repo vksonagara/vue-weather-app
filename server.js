74b552de7058fee8bbd58152eeceb638b458de36
Vinod Sonagara <vksonagara1996@gmail.com>
2018-02-27 23:36:40 +0530

    initial commit

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path')
 // create the express app
 const app = express()
 // create middleware to handle the serving the app
-app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
+app.use('/dist', express.static('dist'))
 // Catch all routes and redirect to the index file
 app.get('*', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.html')
+    res.sendFile(__dirname + '/index.html')
 })
 // Create default port to serve the app on
 const port = process.env.PORT || 5000
